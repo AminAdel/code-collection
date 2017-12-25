787220cddfcfc52a4b504fa8fd3383a9a0a635b1
Amin Adel <AminAdel@users.noreply.github.com>
2017-12-25 11:22:09 +0330

    created toggleSubContent.js

---
+++ JavaScript/toggleSubContent.js
@@ -0,0 +1,18 @@
+//---> Create	:	1396.10.04
+//---> Finish	:	1396.10.04
+//---> Update	:	--
+/*****************************/
+$(document).ready(function () {
+	if ($('.toggleSubContent').length === 0) return;
+	
+	$('.toggleSubContent').click(function(e) {
+		if (e.which !== 1) return;
+		
+		if ($(this).hasClass('open')) {
+			$(this).find('.subContent').fadeOut(100);
+		} else {
+			$(this).find('.subContent').fadeIn(200);
+		}
+		$(this).toggleClass('open');
+	});
+});
