b93513411ae6a28e65b84c13797e12c721708fae
Amin Adel <AminAdel@users.noreply.github.com>
2018-03-12 12:19:06 +0330

    Create addComma.js

---
+++ JavaScript/functions/addComma.js
@@ -0,0 +1,7 @@
+//---> Create	:	1396.08.25
+//---> Finish	:	1396.08.25
+//---> Update	:	--
+/*****************************/
+function addComma(number) {
+	return (new Intl.NumberFormat().format(number));
+}
