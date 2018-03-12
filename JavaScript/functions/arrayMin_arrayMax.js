5f4c221deb9459a8d3779f673e9b2cc6f5bf6717
Amin Adel <AminAdel@users.noreply.github.com>
2018-03-12 12:22:04 +0330

    Create arrayMin_arrayMax.js

---
+++ JavaScript/functions/arrayMin_arrayMax.js
@@ -0,0 +1,10 @@
+//---> Create	:	1395.12.24
+//---> Finish	:	1395.12.24
+//---> Update	:	--
+/*****************************/
+
+var arrayMax = Function.prototype.apply.bind(Math.max, null);
+var arrayMin = Function.prototype.apply.bind(Math.min, null);
+
+// usage : var max = arrayMax([2, 5, 1]);
+// usage : var min = arrayMin([2, 5, 1]);
