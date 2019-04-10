0ecff64c85642cfb9fb5907b9cd4cb23b0a597c1
Amin Adel <AminAdel@users.noreply.github.com>
2019-04-11 01:20:35 +0430

    Create copyToClipboard.js

---
+++ JavaScript/functions/copyToClipboard.js
@@ -0,0 +1,8 @@
+copyToClipboard(item) {
+    document.addEventListener('copy', (e: ClipboardEvent) => {
+      e.clipboardData.setData('text/plain', (item));
+      e.preventDefault();
+      document.removeEventListener('copy', null);
+    });
+    document.execCommand('copy');
+  }
