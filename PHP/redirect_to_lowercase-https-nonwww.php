da8c4533d762cfc072c5fa3fc918b82f79721661
Amin Adel <AminAdel@users.noreply.github.com>
2019-09-11 01:29:32 +0430

    Update redirect_to_lowercase-https-nonwww.php

--- PHP/redirect_to_lowercase-https-nonwww.php
+++ PHP/redirect_to_lowercase-https-nonwww.php
@@ -1,12 +1,19 @@
 <?php
 // redirect to lowercase, https and non-www :
 
+/* START OF REDIRECTION */
 $redirect = false;
 $domain = $_SERVER['HTTP_HOST'];
-if(empty($_SERVER['HTTPS']) || $_SERVER['HTTPS'] == "off") $redirect = true;
-if (substr($_SERVER['HTTP_HOST'], 0, 4) === 'www.') {
+
+// is it https? :
+if(empty($_SERVER['HTTPS']) || $_SERVER['HTTPS'] == "off") {
 	$redirect = true;
+}
+
+// does it have 'www'? :
+if (substr($_SERVER['HTTP_HOST'], 0, 4) === 'www.') {
 	$domain = substr($_SERVER['HTTP_HOST'], 4);
+	$redirect = true;
 }
 
 if ($redirect) {
@@ -14,3 +21,4 @@ if ($redirect) {
 	// header('HTTP/1.1 301 Moved Permanently');
 	header('Location: ' . $location);
 }
+/* END OF REDIRECTION */
