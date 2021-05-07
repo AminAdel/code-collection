29079e7d09ae9641d1f4aba1205c716010171d97
Amin Adel <AminAdel@users.noreply.github.com>
2021-05-07 19:21:08 +0430

    Update addComma.js

--- JavaScript/functions/addComma.js
+++ JavaScript/functions/addComma.js
@@ -1,17 +1,22 @@
 //---> Create	:	1396.08.25
-//---> Finish	:	1396.08.25
+//---> Finish	:	1400.02.17
 //---> Update	:	--
 /*****************************/
-function addComma(num) {
-	// return (new Intl.NumberFormat().format(num)); // this does not work good;
-	let temp =  num.trim();
-	temp = temp.replace(/\D/g,'');
-	temp = temp.split("").reverse(); // array
+function add_comma(num) {
+	num =  num.trim();
+	let num_split = num.split('.');
+	let num1 = num_split[0];
+
+	num1 = num1.replace(/\D/g,'');
+	num1 = num1.split("").reverse(); // array
 	let output_num = '';
-	for (let i = 1; i <= temp.length; i++) {
-		output_num += temp[i-1];
-		if (i % 3 === 0 && i < temp.length) output_num += ',';
+	for (let i = 1; i <= num1.length; i++) {
+		output_num += num1[i-1];
+		if (i % 3 === 0 && i < num1.length) output_num += ',';
 	}
 	output_num = output_num.split("").reverse().join("");
+	if (num_split.length > 1 && parseInt(num_split[1]) > 0) {
+		output_num += '.' + num_split[1]
+	}
 	return output_num;
-}
+} // done
