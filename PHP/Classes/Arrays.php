86bbd760c617bb0a18b781eff47a6f1da342587e
Amin Adel <AminAdel@users.noreply.github.com>
2022-03-25 20:55:37 +0430

    Update Arrays.php

--- PHP/Classes/Arrays.php
+++ PHP/Classes/Arrays.php
@@ -15,24 +15,22 @@ class Arrays
 	public static function merge_recursive_distinct2($array1, $array2) {
 		// keeps indexes of children arrays, but replaces existing values; and removes duplicate items;
 		foreach ($array2 as $index => $item) {
-			if (empty($array1[$index])) {
-				$array1[$index] = $item;
+			if (empty($item)) continue;
+			//==============================
+			if (is_numeric($index)) {
+				if (!in_array($item, $array1)) {
+					$array1[] = $item;
+				}
 				continue;
 			}
-			if (is_numeric($index) && !in_array($item, $array1)) {
-				$array1[] = $item;
+			if (empty($array1[$index]) || !is_array($item)) {
+				$array1[$index] = $item;
 				continue;
 			}
 			if (is_array($item)) {
 				$array1[$index] = self::merge_recursive_distinct2($array1[$index], $item);
 				continue;
 			}
-			/*if (is_array($item) && !empty($array1[$index])) {
-				$array1[$index] = self::merge_recursive_distinct2($array1[$index], $item);
-			}
-			else {
-				$array1[$index] = $item;
-			}*/
 		}
 		return $array1;
 	} // done : 1400.12.29
